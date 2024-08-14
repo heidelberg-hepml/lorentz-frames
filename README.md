c2e8e4cc05737400e6b9b4882541f07ebab43447
Jonas Spinner <jonasspinner@gmx.de>
2024-08-14 21:45:03 +0200

    Basic runnable setup

--- README.md
+++ README.md
@@ -1,3 +1,42 @@
 # lorentz-frames
 
 We adapt the tensorframes of https://arxiv.org/abs/2405.15389 to the Lorentz group.
+
+## 1. Getting started
+
+Clone the repository.
+
+```bash
+git clone https://github.com/heidelberg-hepml/lorentz-frames
+```
+
+Create a virtual environment and install requirements
+
+```bash
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+The datasets can be downloaded from the Heidelberg ITP website ([toptagging](https://www.thphys.uni-heidelberg.de/~plehn/data/toptagging_full.npz)). Finally, keys in the`data` section of the config files have to be adapted to specify where the datasets are located on your machine (`data_path` or `data_dir` depending on the experiment).
+
+## 2. Running experiments
+
+You can run experiments with the following commands:
+```bash
+python run.py -cn toptagging model=gcnconv exp_name=toptagging run_name=hello_world_toptagging
+```
+
+We use hydra for configuration management, allowing to quickly override parameters in e.g. config/toptagging.yaml. Further, we use mlflow for tracking. You can start a mlflow server based on the saved results in runs/tracking/mlflow.db on port 4242 of your machine with the following command
+
+```bash
+mlflow ui --port 4242 --backend-store-uri sqlite:///runs/tracking/mlflow.db
+```
+
+An existing run can be reloaded to perform additional tests with the trained model. For a previous run with exp_name=toptagging and run_name=hello_world_toptagging, one can run for example. 
+```bash
+python run.py -cn config -cp runs/toptagging/hello_world_toptagging train=false warm_start_idx=0
+```
+The warm_start_idx specifies which model in the models folder should be loaded and defaults to 0. 
+
+The default configuration files in the `config` folder define small models to allow quick test runs.
---
+++ experiments/toptagging/dataset.py
@@ -0,0 +1,157 @@
+import torch
+import numpy as np
+from torch_geometric.data import Data
+from torch_geometric.utils import dense_to_sparse
+
+
+class TopTaggingDataset(torch.utils.data.Dataset):
+
+    """
+    We use torch_geometric to handle point cloud of jet constituents more efficiently
+    The torch_geometric dataloader concatenates jets along their constituent direction,
+    effectively combining the constituent index with the batch index in a single dimension.
+    An extra object batch.batch for each batch specifies to which jet the constituent belongs.
+    We extend the constituent list by a global token that is used to embed extra global
+    information and extract the classifier score.
+
+    Structure of the elements in self.data_list
+    x : torch.tensor of shape (num_elements, 4)
+        List of 4-momenta of jet constituents
+    label : torch.tensor of shape (1), dtype torch.int
+        label of the jet (0=QCD, 1=top)
+    is_global : torch.tensor of shape (num_elements), dtype torch.bool
+        True for the global token (first element in constituent list), False otherwise
+        We set is_global=None if no global token is used
+    """
+
+    def __init__(
+        self,
+        filename,
+        mode,
+        cfg,
+        data_scale=None,
+        dtype=torch.float,
+        device="cpu",
+    ):
+        """
+        Parameters
+        ----------
+        filename : str
+            Path to file in npz format where the dataset in stored
+        mode : {"train", "test", "val"}
+            Purpose of the dataset
+            Train, test and validation datasets are already seperated in the specified file
+        cfg : dataclass
+            Dataclass object containing options for the format
+            in which the data is preprocessed at runtime
+        data_scale : float
+            std() of all entries in the train dataset
+            Effectively a change of units to make the network entries O(1)
+        dtype: str
+            Not supported consistently
+        device: torch.device
+            Device on which the dataset will be stored
+        """
+        self.cfg = cfg
+        self.dtype = dtype
+        self.device = device
+
+        data = np.load(filename)
+        kinematics = data[f"kinematics_{mode}"]
+        labels = data[f"labels_{mode}"]
+
+        # preprocessing
+        if mode == "train":
+            data_scale = kinematics.std()
+        else:
+            assert data_scale is not None
+        self.data_scale = data_scale
+
+        if self.cfg.data.rescale_data:
+            kinematics = kinematics / self.data_scale
+        kinematics = torch.tensor(kinematics, dtype=dtype)
+        labels = torch.tensor(labels, dtype=torch.bool)
+
+        # create list of torch_geometric.data.Data objects
+        self.data_list = []
+        for i in range(kinematics.shape[0]):
+            # drop zero-padded components
+            mask = (kinematics[i, ...].abs() > 1e-5).all(dim=-1)
+            x = kinematics[i, ...][mask]
+            label = labels[i, ...]
+
+            # construct global token
+            if self.cfg.data.include_global_token:
+                global_token = torch.zeros_like(x[[0], ...], dtype=self.dtype)
+                global_token[..., 0] = 1
+                x = torch.cat((global_token, x), dim=0)
+                is_global = torch.zeros(x.shape[0], 1, dtype=torch.bool)
+                is_global[0, 0] = True
+            else:
+                is_global = None
+
+            # construct edge_index
+            adj_matrix = torch.ones((x.shape[0], x.shape[0]))
+            edge_index = dense_to_sparse(adj_matrix)[0]
+            data = Data(x=x, label=label, is_global=is_global, edge_index=edge_index)
+            self.data_list.append(data)
+
+    def __len__(self):
+        return len(self.data_list)
+
+    def __getitem__(self, idx):
+        batch = self.data_list[idx].to(self.device)
+
+        # create embeddings
+        if self.cfg.data.add_pt:
+            scalars = get_pt(batch.x)
+        else:
+            scalars = torch.zeros(
+                batch.x.shape[0], 0, device=self.device, dtype=self.dtype
+            )
+        is_global = batch.is_global if self.cfg.data.include_global_token else None
+
+        # add information about which token is global
+        scalars_is_global = torch.zeros(
+            scalars.shape[0], 1, device=self.device, dtype=self.dtype
+        )
+        scalars_is_global[0, :] = 1.0
+        scalars = torch.cat([scalars_is_global, scalars], dim=-1)
+
+        return Data(
+            x=batch.x,
+            scalars=scalars,
+            label=batch.label,
+            is_global=is_global,
+            edge_index=batch.edge_index,
+        )
+
+
+def get_pt(p):
+    # transverse momentum
+    return torch.sqrt(p[..., 1] ** 2 + p[..., 2] ** 2)
+
+
+def _get_phi(p):
+    # azimuthal angle
+    return torch.arctan2(p[..., 2], p[..., 1])
+
+
+def _get_eta(p):
+    # rapidity
+    p_abs = torch.sqrt(torch.sum(p[..., 1:] ** 2, dim=-1))
+    return torch.arctanh(p[..., 3] / p_abs)
+
+
+def get_deltaR(p1, p2):
+    # deltaR = angular distance
+    phi1, phi2 = _get_phi(p1), _get_phi(p2)
+    eta1, eta2 = _get_eta(p1), _get_eta(p2)
+    return torch.sqrt((phi1 - phi2) ** 2 + (eta1 - eta2) ** 2)
+
+
+def get_kt(p1, p2):
+    # un-normalized kt distance, corresponding to R=1
+    pt1, pt2 = get_pt(p1), get_pt(p2)
+    deltaR = get_deltaR(p1, p2)
+    return deltaR * torch.min(pt1, pt2)
