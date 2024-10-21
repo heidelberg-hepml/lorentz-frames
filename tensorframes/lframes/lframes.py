1cb5051ba830f9ef363e38c595d52f757bf7852b
Jonas Spinner <jonasspinner@gmx.de>
2024-10-21 22:01:14 +0200

    Fix device issue for lframes

--- tensorframes/lframes/lframes.py
+++ tensorframes/lframes/lframes.py
@@ -23,7 +23,7 @@ class LFrames:
         self.matrices = matrices
         self.spatial_dim = spatial_dim
 
-        self.metric = torch.diag(torch.tensor([1.0, -1.0, -1.0, -1.0]))
+        self.metric = torch.diag(torch.tensor([1.0, -1.0, -1.0, -1.0], device=self.device))
         self._det = None
         self._inv = None
         self._angles = None
@@ -122,7 +122,7 @@ class ChangeOfLFrames:
         self.matrices = torch.bmm(lframes_end.matrices, lframes_start.inv)
         self.spatial_dim = lframes_start.spatial_dim
 
-        self.metric = torch.diag(torch.tensor([1.0, -1.0, -1.0, -1.0]))
+        self.metric = torch.diag(torch.tensor([1.0, -1.0, -1.0, -1.0], device=self.device))
         self._det = None
         self._inv = None
         self._angles = None
