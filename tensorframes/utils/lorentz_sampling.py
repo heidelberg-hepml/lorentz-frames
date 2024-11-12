06ec37bb2dc64892e5e929ba1d5bc81617caff2e
sebastian <pitz@thphys.uni-heidelberg.de>
2024-11-12 16:53:00 +0100

    Added lorentz sampling function

---
+++ tensorframes/utils/lorentz_sampling.py
@@ -0,0 +1,63 @@
+import torch
+
+
+class sample_trafo:
+    """
+    Sampler class for random lorentz transformations
+    """
+
+    def __init__(
+        self, trafo_types: list, axes: list, mean_eta: float = 0, std_eta: float = 1
+    ):
+        """
+        Initializer for Sampler
+
+        Args:
+            trafo_types (list): List of trafo_types in order, eg. ["rot", "boost", "rot"]
+            axes (list): List of rotation / boost axes in order eg. [[1,2],[0,1],[1,3]]
+            mean_eta (float): mean value for rapidity, used for sampling from normal distribution, defaults to 0
+            std_eta (float): std of rapidity, used for sampleing from normal distribution, defaults to 1
+        """
+        self.trafo_types = trafo_types
+        self.axes = axes
+        self.mean_eta = torch.tensor(mean_eta).to(torch.float)
+        self.std_eta = torch.tensor(std_eta).to(torch.float)
+        self.num_trafo = len(trafo_types)
+
+    def sample(self, N: int = 1):
+        """
+        Sample N transformation matrices with the parameters from the constructor
+
+        Args:
+            N (int): Number of the transformation matrices to sample
+
+        Returns:
+            final_trafo (tensor): tensor of stacked transformation matrices, shape: (batch, 4, 4)
+        """
+        final_trafo = torch.eye(4).unsqueeze(0).repeat(N, 1, 1)
+        for i in range(self.num_trafo):
+            if self.trafo_types[i] == "boost":
+                angle = torch.normal(
+                    mean=self.mean_eta.repeat(N), std=self.std_eta.repeat(N)
+                )
+                temp = torch.eye(4).unsqueeze(0).repeat(N, 1, 1)
+                axes = self.axes[i]
+                temp[:, axes[0], axes[0]] = torch.cosh(angle)
+                temp[:, axes[0], axes[1]] = torch.sinh(angle)
+                temp[:, axes[1], axes[0]] = torch.sinh(angle)
+                temp[:, axes[1], axes[1]] = torch.cosh(angle)
+                final_trafo = torch.einsum("ijk,ikl->ijl", temp, final_trafo)
+            elif self.trafo_types[i] == "rot":
+                angle = torch.rand(N) * 2 * torch.pi
+                temp = torch.eye(4).unsqueeze(0).repeat(N, 1, 1)
+                axes = self.axes[i]
+                temp[:, axes[0], axes[0]] = torch.cos(angle)
+                temp[:, axes[0], axes[1]] = -torch.sin(angle)
+                temp[:, axes[1], axes[0]] = torch.sin(angle)
+                temp[:, axes[1], axes[1]] = torch.cos(angle)
+                final_trafo = torch.einsum("ijk,ikl->ijl", temp, final_trafo)
+            else:
+                assert (
+                    False
+                ), f"Expected trafo_types 'rot' or 'boost', but got {self.trafo_types[i]} instead."
+        return final_trafo
