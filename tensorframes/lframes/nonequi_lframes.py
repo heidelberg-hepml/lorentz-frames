99b204976391b44a016d86585fead7cc65e8fb5e
Jonas Spinner <jonasspinner@gmx.de>
2024-12-30 16:42:49 +0100

    Add non-equivariant LFramesPredictor classes (+ tests that call the constructor)

---
+++ tensorframes/lframes/nonequi_lframes.py
@@ -0,0 +1,57 @@
+import torch
+from tensorframes.lframes.lframes import LFrames
+from tensorframes.utils.transforms import transform, rand_transform
+
+
+class LFramesPredictor(torch.nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self, *args, **kwargs):
+        raise NotImplementedError
+
+
+class IdentityLFrames(LFramesPredictor):
+    """Identity local frames, corresponding to non-equivariant networks"""
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, fourmomenta):
+        return LFrames.global_trafo(fourmomenta.device, n_batch=fourmomenta.shape[0])
+
+
+class RandomLFrames(LFramesPredictor):
+    """Randomly generates a local frame for the whole batch,
+    corresponding to data augmentation."""
+
+    def __init__(self, std_eta=1.0):
+        super().__init__()
+        self.std_eta = std_eta
+
+    def forward(self, fourmomenta):
+        # general random transformation
+        matrix = rand_transform([1], std_eta=self.std_eta, device=fourmomenta.device)
+
+        return LFrames.global_trafo(
+            device=fourmomenta.device, trafo=matrix, n_batch=fourmomenta.shape[0]
+        )
+
+
+class RandomPhiLFrames(LFramesPredictor):
+    """Randomly generates a phi-rotated local frame for the whole batch,
+    corresponding to data augmentation with rotations around the z axis."""
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, fourmomenta):
+        # random rotation around z axis
+        axis = torch.tensor([1, 2], dtype=torch.long, device=fourmomenta.device)
+        axis = axis.unsqueeze(-1)
+        angle = torch.rand(1, device=fourmomenta.device) * 2 * torch.pi
+        matrix = transform([axis], [angle])
+
+        return LFrames.global_trafo(
+            device=fourmomenta.device, trafo=matrix, n_batch=fourmomenta.shape[0]
+        )
---
+++ tests/lframes/test_nonequi_lframes.py
@@ -0,0 +1,36 @@
+import torch
+import pytest
+from tests.constants import TOLERANCES
+
+from tensorframes.reps import TensorReps
+from tensorframes.lframes.nonequi_lframes import (
+    IdentityLFrames,
+    RandomLFrames,
+    RandomPhiLFrames,
+)
+
+
+@pytest.mark.parametrize(
+    "LFramesPredictor", [IdentityLFrames, RandomLFrames, RandomPhiLFrames]
+)
+@pytest.mark.parametrize("shape", [[1000]])
+def test_constructor(LFramesPredictor, shape):
+    fourmomenta = torch.randn(*shape, 4)
+
+    # predict local frames
+    predictor = LFramesPredictor()
+    lframes = predictor(fourmomenta)
+
+    # transform into local frames
+    reps = TensorReps("1x1n")
+    trafo = TensorReps(reps).get_transform_class()
+    x_transformed = trafo(fourmomenta, lframes)
+
+    if LFramesPredictor == IdentityLFrames:
+        # fourmomenta should not change
+        torch.testing.assert_close(x_transformed, fourmomenta, **TOLERANCES)
+    elif LFramesPredictor == RandomPhiLFrames:
+        # energy and pz should not change
+        torch.testing.assert_close(
+            x_transformed[..., [0, 3]], fourmomenta[..., [0, 3]], **TOLERANCES
+        )
