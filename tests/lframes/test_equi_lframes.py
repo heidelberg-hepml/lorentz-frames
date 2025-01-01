0db38952a7fb3f2afa5e765eeadeb92561e081ec
Jonas Spinner <jonasspinner@gmx.de>
2025-01-01 19:34:31 +0100

    Fixed wrong filename in tests

--- tests/lframes/test_equi_lframes.py
+++ tests/lframes/test_equi_lframes.py
@@ -1,10 +1,11 @@
 import torch
 import pytest
-from tests.constants import MILD_TOLERANCES as TOLERANCES, LOGM2_MEAN, LOGM2_STD
+from torch_geometric.utils import dense_to_sparse
+from tests.constants import TOLERANCES, LOGM2_MEAN, LOGM2_STD
 from tests.helpers import sample_vector
 
 from tensorframes.reps import TensorReps
-from tensorframes.lframes.equi_lframes import RestLFrames
+from tensorframes.lframes.equi_lframes import RestLFrames, ReflectLearnedLFrames
 from tensorframes.utils.transforms import rand_transform
 from tensorframes.lframes.lframes import LFrames
 
@@ -42,30 +43,44 @@ def test_lframes_transformation(LFramesPredictor, batch_dims, logm2_std, logm2_m
     torch.testing.assert_close(lframes_prime_estimated, lframes_prime, **TOLERANCES)
 
 
-@pytest.mark.parametrize("LFramesPredictor", [RestLFrames])
-@pytest.mark.parametrize("batch_dims", [[10000]])
+@pytest.mark.parametrize("LFramesPredictor", [ReflectLearnedLFrames])
+@pytest.mark.parametrize("batch_dims", [[2]])
 @pytest.mark.parametrize("logm2_std", LOGM2_STD)
 @pytest.mark.parametrize("logm2_mean", LOGM2_MEAN)
 def test_equivariance(LFramesPredictor, batch_dims, logm2_std, logm2_mean):
     dtype = torch.float64  # required to consistently pass tests
 
     # preparations
-    predictor = LFramesPredictor()
+    if LFramesPredictor == RestLFrames:
+        predictor = LFramesPredictor()
+        call_predictor = lambda fm: predictor(fm)
+    elif LFramesPredictor in [ReflectLearnedLFrames]:
+        assert len(batch_dims) == 1
+        predictor = LFramesPredictor(hidden_channels=[16], in_nodes=0).to(dtype=dtype)
+        edge_index = dense_to_sparse(torch.ones(batch_dims[0], batch_dims[0]))[0]
+        scalars = torch.zeros(*batch_dims, 0, dtype=dtype)
+        call_predictor = lambda fm: predictor(fm, scalars, edge_index)
+
     reps = TensorReps("1x1n")
     trafo = TensorReps(reps).get_transform_class()
 
     # sample Lorentz vectors
     fm = sample_vector(batch_dims, logm2_std, logm2_mean, dtype=dtype)
 
-    # path 1: RestLFrames transform
-    lframes = predictor(fm)
-    fm_local = trafo(fm, lframes)
-
-    # path 2: random transform + RestLFrames transform
     random = rand_transform(batch_dims, dtype=dtype)
+    random_lframes = LFrames(random, is_global=True)
+
+    # path 1: LFrames transform + random transform
+    lframes = call_predictor(fm)
+    fm_local = trafo(fm, lframes)
+    fm_local_prime1 = trafo(fm_local, random_lframes)
+
+    # path 2: random transform + LFrames transform
     fm_prime = torch.einsum("...ij,...j->...i", random, fm)
-    lframes_prime = predictor(fm_prime)
-    fm_local_prime = trafo(fm_prime, lframes_prime)
+    lframes_prime = call_predictor(fm_prime)
+    fm_local_prime2 = trafo(fm_prime, lframes_prime)
 
     # test equivariance condition
-    torch.testing.assert_close(fm_local, fm_local_prime, **TOLERANCES)
+    print(fm_local_prime1)
+    print(fm_local_prime2)
+    torch.testing.assert_close(fm_local_prime1, fm_local_prime2, **TOLERANCES)
