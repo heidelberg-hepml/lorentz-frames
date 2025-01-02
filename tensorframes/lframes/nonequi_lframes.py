e05805b5fb7d487b09a0e4c7f3e040512022f180
Jonas Spinner <jonasspinner@gmx.de>
2025-01-02 19:10:11 +0100

    Add is_global property to lframesnet

--- tensorframes/lframes/nonequi_lframes.py
+++ tensorframes/lframes/nonequi_lframes.py
@@ -4,8 +4,9 @@ from tensorframes.utils.transforms import transform, rand_transform, rand_phirot
 
 
 class LFramesPredictor(torch.nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, is_global=False) -> None:
         super().__init__()
+        self.is_global = is_global
 
     def forward(self, *args, **kwargs):
         raise NotImplementedError
@@ -15,7 +16,7 @@ class IdentityLFrames(LFramesPredictor):
     """Identity local frames, corresponding to non-equivariant networks"""
 
     def __init__(self):
-        super().__init__()
+        super().__init__(is_global=True)
 
     def forward(self, fourmomenta):
         return LFrames.global_trafo(fourmomenta.device, n_batch=fourmomenta.shape[0])
@@ -26,7 +27,7 @@ class RandomLFrames(LFramesPredictor):
     corresponding to data augmentation."""
 
     def __init__(self, std_eta=1.0):
-        super().__init__()
+        super().__init__(is_global=True)
         self.std_eta = std_eta
 
     def forward(self, fourmomenta):
@@ -43,7 +44,7 @@ class RandomPhiLFrames(LFramesPredictor):
     corresponding to data augmentation with rotations around the z axis."""
 
     def __init__(self):
-        super().__init__()
+        super().__init__(is_global=True)
 
     def forward(self, fourmomenta):
         # random rotation around z axis
