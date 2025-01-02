0ed28498fc74eaa8b95a2198259f72ca40256177
Jonas Spinner <jonasspinner@gmx.de>
2025-01-02 12:47:38 +0100

    Fixed lframes transformation behaviour test; RestLFrames still dont pass - have to understand them better

--- tests/lframes/test_equi_lframes.py
+++ tests/lframes/test_equi_lframes.py
@@ -14,37 +14,50 @@ from tensorframes.utils.transforms import rand_transform
 from tensorframes.lframes.lframes import LFrames
 
 
-@pytest.mark.parametrize("LFramesPredictor", [RestLFrames])
-@pytest.mark.parametrize("batch_dims", [[10000]])
-@pytest.mark.parametrize("logm2_std", [1])
-@pytest.mark.parametrize("logm2_mean", [0])
-@pytest.mark.skip(
-    reason="I'm confident that this is implemented correctly,"
-    "but for some reason the test fails - probably numerical precision."
-    "It also doesn't fail totally - it is correct for the large transformation matrix entries"
-)
+@pytest.mark.parametrize(
+    "LFramesPredictor", [ReflectLearnedLFrames, MatrixExpLearnedLFrames]
+)  # RestLFrames dont work yet - have to understand them better
+@pytest.mark.parametrize("batch_dims", [[10]])
+@pytest.mark.parametrize("logm2_std", LOGM2_STD)
+@pytest.mark.parametrize("logm2_mean", LOGM2_MEAN)
 def test_lframes_transformation(LFramesPredictor, batch_dims, logm2_std, logm2_mean):
     dtype = torch.float64  # required to consistently pass tests
 
     # preparations
-    predictor = LFramesPredictor()
+    if LFramesPredictor == RestLFrames:
+        predictor = LFramesPredictor()
+        call_predictor = lambda fm: predictor(fm)
+    elif LFramesPredictor in [ReflectLearnedLFrames, MatrixExpLearnedLFrames]:
+        assert len(batch_dims) == 1
+        predictor = LFramesPredictor(hidden_channels=[16], in_nodes=0).to(dtype=dtype)
+        batch = torch.zeros(batch_dims, dtype=torch.long)
+        edge_index = dense_to_sparse(torch.ones(batch_dims[0], batch_dims[0]))[0]
+        scalars = torch.zeros(*batch_dims, 0, dtype=dtype)
+        call_predictor = lambda fm: predictor(fm, scalars, edge_index, batch)
 
     # sample Lorentz vectors
     fm = sample_vector(batch_dims, logm2_std, logm2_mean, dtype=dtype)
 
     # lframes for un-transformed fm
-    lframes = predictor(fm).inv
+    lframes = call_predictor(fm)
+
+    # random global transformation
+    random = rand_transform([1], dtype=dtype)
+    random = random.repeat(*batch_dims, 1, 1)
 
     # lframes for transformed fm
-    random = rand_transform(batch_dims, dtype=dtype)
     fm_prime = torch.einsum("...ij,...j->...i", random, fm)
-    lframes_prime = predictor(fm_prime).inv
+    lframes_prime = call_predictor(fm_prime)
 
-    # check if lframes transform correctly
+    # check that lframes transform correctly
     # expect lframes_prime = lframes * random^-1
     inv_random = LFrames(random).inv
-    lframes_prime_estimated = torch.einsum("...ij,...jk->...ik", lframes, inv_random)
-    torch.testing.assert_close(lframes_prime_estimated, lframes_prime, **TOLERANCES)
+    lframes_prime_expected = torch.einsum(
+        "...ij,...jk->...ik", lframes.matrices, inv_random
+    )
+    torch.testing.assert_close(
+        lframes_prime_expected, lframes_prime.matrices, **TOLERANCES
+    )
 
 
 @pytest.mark.parametrize(
