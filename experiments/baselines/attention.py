ff92bf2cff00423857b358467f2db89ba9715ea9
Jonas Spinner <jonasspinner@gmx.de>
2025-01-02 15:51:04 +0100

    Add baseline networks (not interfaced with main code yet)

---
+++ experiments/baselines/attention.py
@@ -0,0 +1,62 @@
+from typing import Optional, Union
+
+from torch import Tensor
+from torch.nn.functional import scaled_dot_product_attention as torch_sdpa
+from xformers.ops import AttentionBias, memory_efficient_attention
+
+# Masked out attention logits are set to this constant (a finite replacement for -inf):
+_MASKED_OUT = float("-inf")
+
+# Force the use of xformers attention, even when no xformers attention mask is provided:
+FORCE_XFORMERS = False
+
+def scaled_dot_product_attention(
+    query: Tensor,
+    key: Tensor,
+    value: Tensor,
+    attn_mask: Optional[Union[AttentionBias, Tensor]] = None,
+    is_causal=False,
+) -> Tensor:
+    """Execute (vanilla) scaled dot-product attention.
+
+    Dynamically dispatch to xFormers if attn_mask is an instance of xformers.ops.AttentionBias
+    or FORCE_XFORMERS is set, use torch otherwise.
+
+    Parameters
+    ----------
+    query : Tensor
+        of shape [batch, head, item, d]
+    key : Tensor
+        of shape [batch, head, item, d]
+    value : Tensor
+        of shape [batch, head, item, d]
+    attn_mask : Optional[Union[AttentionBias, Tensor]]
+        Attention mask
+    is_causal: bool
+
+    Returns
+    -------
+    Tensor
+        of shape [batch, head, item, d]
+    """
+    if FORCE_XFORMERS or isinstance(attn_mask, AttentionBias):
+        assert (
+            not is_causal
+        ), "is_causal=True not implemented yet for xformers attention"
+        if key.shape[1] != query.shape[1]:  # required to make multi_query work
+            key = key.expand(key.shape[0], query.shape[1], *key.shape[2:])
+            value = value.expand(value.shape[0], query.shape[1], *value.shape[2:])
+        query = query.transpose(
+            1, 2
+        )  # [batch, head, item, d] -> [batch, item, head, d]
+        key = key.transpose(1, 2)
+        value = value.transpose(1, 2)
+        out = memory_efficient_attention(
+            query.contiguous(),
+            key.contiguous(),
+            value,
+            attn_bias=attn_mask,
+        )
+        out = out.transpose(1, 2)  # [batch, item, head, d] -> [batch, head, item, d]
+        return out
+    return torch_sdpa(query, key, value, attn_mask=attn_mask, is_causal=is_causal)
