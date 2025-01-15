e0e7c1d9d2a2a61963b4914b41fc03d8bcc28f7d
sebastian <pitz@thphys.uni-heidelberg.de>
2025-01-15 14:03:50 +0100

    updated unit tests for orthogonality to check that there only is one time-like vector and work with collinear and light-like vectors

--- tests/utils/test_orthogonalize.py
+++ tests/utils/test_orthogonalize.py
@@ -2,7 +2,10 @@ import torch
 import pytest
 from tests.constants import TOLERANCES, BATCH_DIMS
 
-from tensorframes.utils.lorentz import lorentz_inner
+from tensorframes.utils.lorentz import (
+    lorentz_inner,
+    lorentz_squarednorm,
+)
 from tensorframes.utils.orthogonalize import (
     lorentz_cross,
     orthogonalize_cross,
@@ -31,13 +34,23 @@ def test_lorentz_cross(batch_dims):
 
 
 @pytest.mark.parametrize("batch_dims", BATCH_DIMS)
-def test_orthogonalize_random_vectors(batch_dims):
+def test_orthogonalize(batch_dims):
     v1 = torch.randn(batch_dims + [4])
     v2 = torch.randn(batch_dims + [4])
     v3 = torch.randn(batch_dims + [4])
 
     orthogonal_vecs = orthogonalize_cross([v1, v2, v3])
 
+    # test if there is only one time-like vector in each set of orthogonalized vectors
+    norm = torch.stack([lorentz_squarednorm(v) for v in orthogonal_vecs], dim=-1)
+    pos_norm = norm > 0
+    torch.testing.assert_close(
+        torch.sum(pos_norm, dim=-1),
+        torch.ones(batch_dims).to(torch.int64),
+        atol=0,
+        rtol=0,
+    )
+
     for i1, v1 in enumerate(orthogonal_vecs):
         for i2, v2 in enumerate(orthogonal_vecs):
             inner = lorentz_inner(v1, v2)
@@ -45,11 +58,13 @@ def test_orthogonalize_random_vectors(batch_dims):
             torch.testing.assert_close(inner.abs(), target, **TOLERANCES)
 
 
-@pytest.mark.parametrize("batch_dims", BATCH_DIMS)
-def test_orthogonalize_identical_vectors(batch_dims):
+@pytest.mark.parametrize("batch_dims", [[10000]])
+@pytest.mark.parametrize("eps", [1e-10, 1e-5, 1e-2])
+def test_orthogonalize_collinear(batch_dims, eps):
+    # test for collinear (and also coplanar) vectors
     v1 = torch.randn(batch_dims + [4])
     v2 = torch.randn(batch_dims + [4])
-    v3 = v1.clone()
+    v3 = v1.clone() + eps * torch.randn(batch_dims + [4])
 
     orthogonal_vecs = orthogonalize_cross([v1, v2, v3])
 
@@ -60,25 +75,14 @@ def test_orthogonalize_identical_vectors(batch_dims):
             torch.testing.assert_close(inner.abs(), target, **TOLERANCES)
 
 
-@pytest.mark.parametrize("batch_dims", BATCH_DIMS)
-def test_orthogonalize_collinear_vectors(batch_dims):
-    v1 = torch.randn(batch_dims + [4])
-    v2 = torch.randn(batch_dims + [4])
-    v3 = torch.randn(batch_dims + [1]) * v1 + torch.randn(batch_dims + [1]) * v2
-
-    orthogonal_vecs = orthogonalize_cross([v1, v2, v3])
-
-    for i1, v1 in enumerate(orthogonal_vecs):
-        for i2, v2 in enumerate(orthogonal_vecs):
-            inner = lorentz_inner(v1, v2)
-            target = torch.ones_like(inner) if i1 == i2 else torch.zeros_like(inner)
-            torch.testing.assert_close(inner.abs(), target, **TOLERANCES)
-
-
-@pytest.mark.parametrize("batch_dims", BATCH_DIMS)
-def test_orthogonalize_lightlike_vector(batch_dims):
+@pytest.mark.parametrize("batch_dims", [[10000]])
+@pytest.mark.parametrize("eps", [1e-10, 1e-5, 1e-2])
+def test_orthogonalize_lightlike(batch_dims, eps):
+    # test for a lightlike vector
     temp = torch.randn(batch_dims + [3])
-    temp2 = torch.linalg.norm(temp, dim=-1, keepdim=True)
+    temp2 = torch.linalg.norm(temp, dim=-1, keepdim=True) + eps * torch.randn(
+        batch_dims + [1]
+    )
     v1 = torch.cat((temp2, temp), dim=-1)
     v2 = torch.randn(batch_dims + [4])
     v3 = torch.randn(batch_dims + [4])
