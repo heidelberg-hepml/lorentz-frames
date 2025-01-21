fd5d45419b0c3db9dff2176cbb1e1846db9aaa02
Jonas Spinner <jonasspinner@gmx.de>
2025-01-21 09:48:59 +0100

    Add JetClass experiment

--- run.py
+++ run.py
@@ -1,11 +1,14 @@
 import hydra
 from experiments.tagging.experiment import TopTaggingExperiment
+from experiments.tagging.jetclassexperiment import JetClassTaggingExperiment
 
 
 @hydra.main(config_path="config_quick", config_name="toptagging", version_base=None)
 def main(cfg):
     if cfg.exp_type == "toptagging":
         exp = TopTaggingExperiment(cfg)
+    elif cfg.exp_type == "jctagging":
+        exp = JetClassTaggingExperiment(cfg)
     else:
         raise ValueError(f"exp_type {cfg.exp_type} not implemented")
 
