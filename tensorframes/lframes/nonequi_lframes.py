02757b1c091eb10c311c6f2e579f25d50110e8ba
Jonas Spinner <jonasspinner@gmx.de>
2025-02-21 14:11:15 +0100

    Add RandomBoostLFrames and extend documentation in nonequi_lframes.py

--- tensorframes/lframes/nonequi_lframes.py
+++ tensorframes/lframes/nonequi_lframes.py
@@ -1,6 +1,11 @@
 import torch
 from tensorframes.lframes.lframes import LFrames
-from tensorframes.utils.transforms import rand_lorentz, rand_phirotation, rand_rotation
+from tensorframes.utils.transforms import (
+    rand_lorentz,
+    rand_rotation,
+    rand_phirotation,
+    rand_boost,
+)
 
 
 class LFramesPredictor(torch.nn.Module):
@@ -54,14 +59,14 @@ class RandomLFrames(LFramesPredictor):
 
 
 class RandomRotLFrames(LFramesPredictor):
-    """Randomly generates a rotated local frame for the whole batch,
+    """Randomly generates a rotated local frame,
     corresponding to data augmentation."""
 
     def __init__(self, std_eta=1.0):
         super().__init__(is_global=True)
 
     def forward(self, fourmomenta, return_tracker=False):
-        # general random transformation
+        # random rotation
         shape = fourmomenta.shape[:-2] + (1,)
         matrix = rand_rotation(shape, device=fourmomenta.device)
         matrix = matrix.expand(*fourmomenta.shape[:-1], 4, 4)
@@ -76,7 +81,7 @@ class RandomRotLFrames(LFramesPredictor):
 
 
 class RandomPhiLFrames(LFramesPredictor):
-    """Randomly generates a phi-rotated local frame for the whole batch,
+    """Randomly generates a phi-rotated local frame,
     corresponding to data augmentation with rotations around the z axis."""
 
     def __init__(self):
@@ -86,6 +91,29 @@ class RandomPhiLFrames(LFramesPredictor):
         # random rotation around z axis
         shape = fourmomenta.shape[:-2] + (1,)
         matrix = rand_phirotation(shape, device=fourmomenta.device)
+        matrix = matrix.expand(*fourmomenta.shape[:-1], 4, 4)
+
+        lframes = LFrames(
+            is_global=True,
+            matrices=matrix,
+            device=fourmomenta.device,
+            dtype=fourmomenta.dtype,
+        )
+        return (lframes, {}) if return_tracker else lframes
+
+
+class RandomBoostLFrames(LFramesPredictor):
+    """Randomly generates a boosted local frame,
+    corresponding to data augmentation."""
+
+    def __init__(self, std_eta=1.0):
+        super().__init__(is_global=True)
+        self.std_eta = std_eta
+
+    def forward(self, fourmomenta, return_tracker=False):
+        # random boost
+        shape = fourmomenta.shape[:-2] + (1,)
+        matrix = rand_boost(shape, std_eta=self.std_eta, device=fourmomenta.device)
         matrix = matrix.expand(*fourmomenta.shape[:-1], 4, 4)
 
         lframes = LFrames(
