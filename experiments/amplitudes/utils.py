94f0ec167d79339f9403abc6d01e80b16ee9fdeb
Jonas Spinner <jonasspinner@gmx.de>
2025-02-26 20:40:16 +0100

    Add encode_event function

--- experiments/amplitudes/utils.py
+++ experiments/amplitudes/utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from tensorframes.utils.lorentz import lorentz_squarednorm
+
 
 def preprocess_momentum(momentum, mean=None, std=None):
     if mean is None or std is None:
@@ -24,3 +26,13 @@ def undo_preprocess_amplitude(prepd_amplitude, mean, std):
     log_amplitude = prepd_amplitude * std + mean
     amplitude = np.exp(log_amplitude.clamp(min=-10, max=10))
     return amplitude
+
+
+def encode_event(fourmomenta, n_in=2):
+    fourmomenta_in = fourmomenta[..., :n_in, :]
+    fourmomenta_out = fourmomenta[..., n_in:, :]
+
+    initial_state = fourmomenta_in.sum(dim=-2)
+    in_invariant = lorentz_squarednorm(initial_state)
+    in_invariant = in_invariant.clamp(min=1e-10).log()
+    return in_invariant, fourmomenta_out
