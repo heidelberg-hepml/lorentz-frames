Fix 3 mistakes with amplitudes data.mass_reg: change default to data.mass_reg=1e0 (have units=400 for amplitudes, so mass=1e-2 turns into almost mass=1e-5 with mass**2 effectively =0); use 'mass' instead of 'data.mass_reg' to modify momentum[..., 0]; apply sqrt() to get momentum[..., 0]

experiments/amplitudes/utils.py
@@ -52,7 +52,7 @@ def load_file(data_path, cfg_data, dataset, momentum_std=None, dtype=torch.float
     if cfg_data.mass_reg is not None:
         mass = get_mass(dataset, cfg_data.mass_reg)
         mass = torch.tensor(mass, dtype=dtype).unsqueeze(0)
-        momentum[..., 0] = (momentum[..., 1:] ** 2).sum(dim=-1) + cfg_data.mass_reg**2
+        momentum[..., 0] = torch.sqrt((momentum[..., 1:] ** 2).sum(dim=-1) + mass**2)
 
     # prepare momenta
     if cfg_data.prepare == "align":