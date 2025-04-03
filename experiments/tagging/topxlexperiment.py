66e5dfb957da1c92d2e57fb8d6b57fa0fb5e162b
Jonas Spinner <jonasspinner@gmx.de>
2025-04-03 09:15:39 +0200

    Unify the data.data_config / data.features solution for jetclass and topxl

--- experiments/tagging/topxlexperiment.py
+++ experiments/tagging/topxlexperiment.py
@@ -1,6 +1,5 @@
 import torch
 from torch.utils.data import DataLoader
-from omegaconf import open_dict
 
 import os, time
 
@@ -19,33 +18,32 @@ from experiments.tagging.miniweaver.loader import to_filelist
 class TopXLTaggingExperiment(TaggingExperiment):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        with open_dict(self.cfg):
-            self.cfg.model.out_channels = 1
-            self.cfg.model.in_channels = 4  # energy-momentum vector
+        self.cfg.model.out_channels = 1
+        self.cfg.model.in_channels = 4  # energy-momentum vector
 
-            if self.cfg.data.features == "fourmomenta":
-                self.cfg.data.data_config = (
-                    "experiments/tagging/miniweaver/configs_topxl/fourmomenta.yaml"
-                )
-            elif self.cfg.data.features == "pid":
-                self.cfg.model.in_channels += 6
-                self.cfg.data.data_config = (
-                    "experiments/tagging/miniweaver/configs_topxl/pid.yaml"
-                )
-            elif self.cfg.data.features == "displacements":
-                self.cfg.model.in_channels += 4
-                self.cfg.data.data_config = (
-                    "experiments/tagging/miniweaver/configs_topxl/displacements.yaml"
-                )
-            elif self.cfg.data.features == "default":
-                self.cfg.model.in_channels += 10
-                self.cfg.data.data_config = (
-                    "experiments/tagging/miniweaver/configs_topxl/default.yaml"
-                )
-            else:
-                raise ValueError(
-                    f"Input feature option {self.cfg.data.features} not implemented"
-                )
+        if self.cfg.data.features == "fourmomenta":
+            self.cfg.data.data_config = (
+                "experiments/tagging/miniweaver/configs_topxl/fourmomenta.yaml"
+            )
+        elif self.cfg.data.features == "pid":
+            self.cfg.model.in_channels += 6
+            self.cfg.data.data_config = (
+                "experiments/tagging/miniweaver/configs_topxl/pid.yaml"
+            )
+        elif self.cfg.data.features == "displacements":
+            self.cfg.model.in_channels += 4
+            self.cfg.data.data_config = (
+                "experiments/tagging/miniweaver/configs_topxl/displacements.yaml"
+            )
+        elif self.cfg.data.features == "default":
+            self.cfg.model.in_channels += 10
+            self.cfg.data.data_config = (
+                "experiments/tagging/miniweaver/configs_topxl/default.yaml"
+            )
+        else:
+            raise ValueError(
+                f"Input feature option {self.cfg.data.features} not implemented"
+            )
 
     def init_physics(self):
         # decide which entries to use for the lframesnet
