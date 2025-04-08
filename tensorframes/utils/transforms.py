90baff45b66a703e3ee8ecd2750c79595b1e49b9
Jonas Spinner <jonasspinner@gmx.de>
2025-04-08 15:39:02 +0200

    Better data augmentations (#36)
    * Add rand_rotation_uniform function (should become default later on)
    
    * Use rand_rotation_uniform everywhere; rename rand_rotation -> rand_rotation_naive
    
    * Add a truncated Gaussian distribution for the boost angles to avoid outliers
    
    * More descriptive name; Add same transformation for randomztransform
    
    ---------
    
    Co-authored-by: Luigi Favaro <luigi.favaro@hotmail.it>

--- tensorframes/utils/transforms.py
+++ tensorframes/utils/transforms.py
@@ -64,6 +64,7 @@ def transform(
 def rand_lorentz(
     shape: List[int],
     std_eta: float = 0.5,
+    n_max_std_eta: float = 5.0,
     device: str = "cpu",
     dtype: torch.dtype = torch.float32,
     generator: torch.Generator = None,
@@ -80,6 +81,9 @@ def rand_lorentz(
             Shape of the transformation matrices
         std_eta: float
             Standard deviation of rapidity
+        n_max_std_eta: float
+            Allowed number of standard deviations;
+            used to sample from a truncated Gaussian
         device: str
         dtype: torch.dtype
         generator: torch.Generator
@@ -90,17 +94,23 @@ def rand_lorentz(
     assert std_eta > 0
     ones = torch.ones(shape, device=device, dtype=torch.long)
     axis = torch.stack([0 * ones, 1 * ones], dim=0)
-    angle = (
-        torch.randn(*shape, device=device, dtype=dtype, generator=generator) * std_eta
+    angle = sample_rapidity(
+        shape,
+        std_eta=std_eta,
+        n_max_std_eta=n_max_std_eta,
+        device=device,
+        dtype=dtype,
+        generator=generator,
     )
+
     boost = transform([axis], [angle])
 
-    rotation = rand_rotation(shape, device, dtype, generator=generator)
+    rotation = rand_rotation_uniform(shape, device, dtype, generator=generator)
     trafo = torch.einsum("...ij,...jk,...kl->...il", boost, rotation, boost)
     return trafo
 
 
-def rand_rotation(
+def rand_rotation_naive(
     shape: List[int],
     device: str = "cpu",
     dtype: torch.dtype = torch.float32,
@@ -113,6 +123,9 @@ def rand_rotation(
 
     Note that 3 rotations are enough to cover the whole group (Euler angles)
     (two rotations are only enough for point masses)
+
+    Also, note that these rotations are not uniform. For uniform rotations,
+    use rand_rotation_uniform (which is probably more powerful).
 
     Args:
         shape: List[int]
@@ -151,7 +164,7 @@ def rand_xyrotation(
 ):
     """
     Create N xy-plane rotation matrices embedded in the Lorentz group
-    This function is a special case of rand_rotation
+    This function is a special case of rand_rotation_naive
 
     Args:
         shape: List[int]
@@ -176,6 +189,7 @@ def rand_xyrotation(
 def rand_ztransform(
     shape: List[int],
     std_eta: float = 0.5,
+    n_max_std_eta: float = 5.0,
     device: str = "cpu",
     dtype: torch.dtype = torch.float32,
     generator: torch.Generator = None,
@@ -189,6 +203,9 @@ def rand_ztransform(
             Shape of the transformation matrices
         std_eta: float
             Standard deviation of rapidity
+        n_max_std_eta: float
+            Allowed number of standard deviations;
+            used to sample from a truncated Gaussian
         device: str
         dtype: torch.dtype
         generator: torch.Generator
@@ -208,8 +225,83 @@ def rand_ztransform(
     # boost along z-axis
     axis2 = torch.tensor([0, 3], dtype=torch.long, device=device)
     axis2 = axis2.view(2, *([1] * len(shape))).repeat(1, *shape)
-    angle2 = (
-        torch.rand(*shape, device=device, dtype=dtype, generator=generator) * std_eta
+    angle2 = sample_rapidity(
+        shape,
+        std_eta=std_eta,
+        n_max_std_eta=n_max_std_eta,
+        device=device,
+        dtype=dtype,
+        generator=generator,
     )
 
     return transform([axis1, axis2], [angle1, angle2])
+
+
+def rand_rotation_uniform(
+    shape: List[int],
+    device: str = "cpu",
+    dtype: torch.dtype = torch.float32,
+    generator: torch.Generator = None,
+):
+    """
+    Create N rotation matrices embedded in the Lorentz group
+    using quaternions. In contrast to the rand_rotation_naive
+    function above, this is formally sound uniform sampling.
+
+    Args:
+        shape: List[int]
+            Shape of the transformation matrices
+        device: str
+        dtype: torch.dtype
+        generator: torch.Generator
+
+    Returns:
+        final_trafo: torch.tensor of shape (*shape, 4, 4)
+    """
+    # generate random quaternions
+    u = torch.rand(*shape, 3, device=device, dtype=dtype, generator=generator)
+    q1 = torch.sqrt(1 - u[..., 0]) * torch.sin(2 * torch.pi * u[..., 1])
+    q2 = torch.sqrt(1 - u[..., 0]) * torch.cos(2 * torch.pi * u[..., 1])
+    q3 = torch.sqrt(u[..., 0]) * torch.sin(2 * torch.pi * u[..., 2])
+    q0 = torch.sqrt(u[..., 0]) * torch.cos(2 * torch.pi * u[..., 2])
+
+    # create rotation matrix from quaternions
+    R1 = torch.stack(
+        [1 - 2 * (q2**2 + q3**2), 2 * (q1 * q2 - q0 * q3), 2 * (q1 * q3 + q0 * q2)],
+        dim=-1,
+    )
+    R2 = torch.stack(
+        [2 * (q1 * q2 + q0 * q3), 1 - 2 * (q1**2 + q3**2), 2 * (q2 * q3 - q0 * q1)],
+        dim=-1,
+    )
+    R3 = torch.stack(
+        [2 * (q1 * q3 - q0 * q2), 2 * (q2 * q3 + q0 * q1), 1 - 2 * (q1**2 + q2**2)],
+        dim=-1,
+    )
+    R = torch.stack([R1, R2, R3], dim=-2)
+
+    trafo = torch.eye(4, device=device, dtype=dtype).expand(*shape, 4, 4).clone()
+    trafo[..., 1:, 1:] = R
+    return trafo
+
+
+def sample_rapidity(
+    shape,
+    std_eta,
+    n_max_std_eta=5.0,
+    device="cpu",
+    dtype=torch.float32,
+    generator=None,
+):
+    angle = (
+        torch.randn(*shape, device=device, dtype=dtype, generator=generator) * std_eta
+    )
+    truncate_mask = torch.abs(angle) > std_eta * n_max_std_eta
+    while truncate_mask.any():
+        new_angle = (
+            torch.randn(*shape, device=device, dtype=dtype, generator=generator)
+            * std_eta
+        )
+        angle[truncate_mask] = new_angle[truncate_mask]
+        truncate_mask = torch.abs(angle) > std_eta * n_max_std_eta
+    return angle
