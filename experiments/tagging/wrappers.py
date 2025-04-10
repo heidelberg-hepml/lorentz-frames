31cbf88c64c55beee7875dbf13615db1ee2f3fec
Jonas Spinner <jonasspinner@gmx.de>
2025-04-10 19:47:00 +0200

    Fix ParT dtype bug (small mistake)

--- experiments/tagging/wrappers.py
+++ experiments/tagging/wrappers.py
@@ -252,6 +252,7 @@ class BaselineParTWrapper(TaggerWrapper):
             batch,
             tracker,
         ) = super().forward(embedding)
+        fourmomenta_local = fourmomenta_local.to(features_local.dtype)
         fourmomenta_local = fourmomenta_local[..., [1, 2, 3, 0]]  # need (px, py, pz, E)
 
         features_local, mask = to_dense_batch(features_local, batch)
