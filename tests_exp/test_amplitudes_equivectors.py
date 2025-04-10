b35ae6308fdc5342bb0e08d3a4d7e311738cea32
sebastian <pitz@thphys.uni-heidelberg.de>
2025-04-10 14:01:26 +0200

    add tests for equivectors

---
+++ tests_exp/test_amplitudes_equivectors.py
@@ -0,0 +1,113 @@
+import torch
+import pytest
+import hydra
+import numpy as np
+import os
+
+import experiments.logger
+from experiments.amplitudes.experiment import AmplitudeExperiment
+from tensorframes.utils.transforms import rand_rotation_uniform, rand_lorentz
+
+
+@pytest.mark.parametrize(
+    "model_idx,model_list",
+    list(
+        enumerate(
+            [
+                ["model=amp_mlp"],
+                ["model=amp_transformer"],
+                ["model=amp_graphnet"],
+                ["model=amp_graphnet", "model.include_edges=false"],
+                ["model=amp_graphnet", "model.include_nodes=false"],
+            ],
+        )
+    ),
+)
+@pytest.mark.parametrize("lframesnet", ["orthogonal", "polardec"])
+@pytest.mark.parametrize("rand_trafo", [rand_rotation_uniform, rand_lorentz])
+@pytest.mark.parametrize("iterations", [100])
+@pytest.mark.parametrize("use_float64", [False, True])
+def test_amplitudes(
+    model_idx,
+    model_list,
+    lframesnet,
+    rand_trafo,
+    iterations,
+    use_float64,
+    save=False,
+):
+    experiments.logger.LOGGER.disabled = True  # turn off logging
+
+    # create experiment environment
+    with hydra.initialize(config_path="../config_quick", version_base=None):
+        overrides = [
+            *model_list,
+            f"model/lframesnet={lframesnet}",
+            "save=false",
+            f"use_float64={use_float64}",
+            # "training.batchsize=1",
+        ]
+        cfg = hydra.compose(config_name="amplitudes", overrides=overrides)
+        exp = AmplitudeExperiment(cfg)
+    exp._init()
+    exp.init_physics()
+    exp.init_model()
+    exp.init_data()
+    exp._init_dataloader()
+    exp._init_loss()
+
+    def cycle(iterable):
+        while True:
+            for x in iterable:
+                yield x
+
+    mses = []
+    infs = []
+    iterator = iter(cycle(exp.train_loader))
+    for _ in range(iterations):
+        data = next(iterator)
+        mom = data[1].to(exp.device)
+
+        particle_type = exp.model.encode_particle_type(mom.shape[0]).to(
+            dtype=exp.model.input_dtype, device=mom.device
+        )
+        vecs = exp.model.lframesnet.equivectors(
+            mom, scalars=particle_type, ptr=None
+        )
+        
+
+        # augmented data
+        trafo = rand_trafo(mom.shape[:-2]).to(exp.device)
+        mom_augmented = torch.einsum(
+            "...nm,...vm->...vn", trafo.to(torch.float64), mom.to(torch.float64)
+        ).to(exp.dtype)
+
+        particle_type_augmented = exp.model.encode_particle_type(mom_augmented.shape[0]).to(
+            dtype=exp.model.input_dtype, device=mom_augmented.device
+        )
+        vecs_augmented = exp.model.lframesnet.equivectors(
+            mom_augmented, scalars=particle_type_augmented, ptr=None
+        )
+
+        vecs_augmented = torch.einsum(
+            "...nm,...vim->...vin", torch.linalg.inv(trafo.to(torch.float64)), vecs_augmented.to(torch.float64)
+        ).to(exp.dtype)
+        
+        norm = vecs+vecs_augmented
+        infs.append(torch.any(torch.isinf(norm)).item())
+        
+        mse = ((vecs-vecs_augmented)/norm).pow(2).mean(dim=-2)
+        mses.append(mse.mean(dim=-2))
+    mses = torch.cat(mses, dim=0).clamp(min=1e-30)
+    print("infs: ", infs)
+    print(
+        f"log-mean={mses.log().mean().exp():.2e} max={mses.max().item():.2e}",
+        model_list,
+        rand_trafo.__name__,
+        lframesnet,
+        "float64" if use_float64 else "float32",
+    )
+    if save:
+        os.makedirs("scripts/equi-violation", exist_ok=True)
+        filename = f"scripts/equi-violation/equitest_amp_equivectors_{model_idx}_{lframesnet}_{rand_trafo.__name__}_{'float64' if use_float64 else 'float32'}.npy"
+        np.save(filename, mses.cpu().numpy())
---
+++ tests_exp/test_tagging_equivectors.py
@@ -0,0 +1,162 @@
+import torch
+import pytest
+import hydra
+import numpy as np
+import os
+
+import experiments.logger
+from experiments.tagging.experiment import TopTaggingExperiment
+from tensorframes.utils.transforms import (
+    rand_rotation_uniform,
+    rand_lorentz,
+    rand_xyrotation,
+)
+from experiments.tagging.embedding import embed_tagging_data
+from torch_geometric.nn.aggr import MeanAggregation
+
+
+@pytest.mark.parametrize(
+    "rand_trafo,breaking_list",
+    [
+        [
+            rand_rotation_uniform,
+            ["data.beam_reference=null", "data.add_time_reference=false"],
+        ],
+        [rand_lorentz, ["data.beam_reference=null", "data.add_time_reference=false"]],
+        [rand_xyrotation, ["data.beam_reference=null", "data.add_time_reference=false"]],
+    ],
+)
+@pytest.mark.parametrize(
+    "model_idx,model_list",
+    list(
+        enumerate(
+            [
+                ["model=tag_particlenet-lite"],
+                #["model=tag_transformer"],
+                #["model=tag_graphnet"],
+                #["model=tag_graphnet", "model.include_edges=false"],
+            ]
+        )
+    ),
+)
+@pytest.mark.parametrize("lframesnet", ["orthogonal", "polardec"])
+@pytest.mark.parametrize("iterations", [10])
+@pytest.mark.parametrize("use_float64", [False, True])
+def test_amplitudes(
+    rand_trafo,
+    model_idx,
+    model_list,
+    lframesnet,
+    breaking_list,
+    iterations,
+    use_float64,
+    save=False,
+):
+    experiments.logger.LOGGER.disabled = True  # turn off logging
+
+    # create experiment environment
+    with hydra.initialize(config_path="../config_quick", version_base=None):
+        overrides = [
+            *model_list,
+            f"model/lframesnet={lframesnet}",
+            "save=false",
+            *breaking_list,
+            f"use_float64={use_float64}",
+            # "training.batchsize=1",
+        ]
+        cfg = hydra.compose(config_name="toptagging", overrides=overrides)
+        exp = TopTaggingExperiment(cfg)
+    exp._init()
+    exp.init_physics()
+    exp.init_model()
+    exp.init_data()
+    exp._init_dataloader()
+    exp._init_loss()
+
+    def cycle(iterable):
+        while True:
+            for x in iterable:
+                yield x
+
+    mses = []
+    infs = []
+    agg = MeanAggregation()
+    iterator = iter(cycle(exp.train_loader))
+    for _ in range(iterations):
+        data = next(iterator).to(exp.device)
+        data_augmented = data.clone().to(exp.device)
+
+        embedded_data = embed_tagging_data(
+            data.x.to(exp.dtype),
+            data.scalars.to(exp.dtype),
+            data.ptr,
+            exp.cfg.data,
+        )
+
+        fourmomenta = embedded_data['fourmomenta']
+        global_tagging_features = embedded_data['global_tagging_features']
+        scalars = embedded_data['scalars']
+        ptr = embedded_data['ptr']
+        batch_nospurions = embedded_data['batch']
+
+        assert fourmomenta.shape[0] == data.x.shape[0] # no spurions
+        scalars_withspurions = torch.cat(
+        [scalars, global_tagging_features], dim=-1
+        )
+        vecs = exp.model.lframesnet.equivectors(
+            fourmomenta,
+            scalars=scalars_withspurions,
+            ptr=ptr,
+        )
+
+        # augmented data
+        mom = data_augmented.x.to(exp.device)
+        trafo = rand_trafo(mom.shape[:-2] + (1,)).to(exp.device)
+        mom_augmented = torch.einsum(
+            "...ij,...j->...i", trafo.to(torch.float64), mom.to(torch.float64)
+        ).to(exp.dtype)
+        data_augmented.x = mom_augmented
+
+        embedded_data_augmented = embed_tagging_data(
+            data_augmented.x.to(exp.dtype),
+            data_augmented.scalars.to(exp.dtype),
+            data_augmented.ptr,
+            exp.cfg.data,
+        )
+
+        fourmomenta_augmented = embedded_data_augmented['fourmomenta']
+        global_tagging_features_augmented = embedded_data_augmented['global_tagging_features']
+        scalars_augmented = embedded_data_augmented['scalars']
+        scalars_withspurions_augmented = torch.cat(
+        [scalars_augmented, global_tagging_features_augmented], dim=-1
+        )
+        assert fourmomenta_augmented.shape[0] == data.x.shape[0] # no spurions
+        vecs_augmented = exp.model.lframesnet.equivectors(
+            fourmomenta_augmented,
+            scalars=scalars_withspurions_augmented,
+            ptr=ptr,
+        )
+
+        vecs_augmented = torch.einsum(
+            "...ij,...j->...i", torch.linalg.inv(trafo.to(torch.float64)), vecs_augmented.to(torch.float64)
+        ).to(exp.dtype)
+
+        norm = vecs+vecs_augmented
+        infs.append(torch.any(torch.isinf(norm)).item())
+        
+        mse = ((vecs-vecs_augmented)/norm).pow(2).mean(dim=-2)
+        mses.append(agg(mse, index=batch_nospurions))
+
+    mses = torch.cat(mses, dim=0)
+    print("infs: ", infs)
+    print(
+        f"log-mean={mses.log().mean().exp():.2e} max={mses.max().item():.2e}",
+        model_list,
+        rand_trafo.__name__,
+        lframesnet,
+        "float64" if use_float64 else "float32",
+    )
+    if save:
+        os.makedirs("scripts/equi-violation", exist_ok=True)
+        filename = f"scripts/equi-violation/equitest_tag_equivectors_{model_idx}_{lframesnet}_{rand_trafo.__name__}_{'float64' if use_float64 else 'float32'}.npy"
+        np.save(filename, mses.cpu().numpy())
