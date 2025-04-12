dc082bf03059c2315d8b8dde5f36e7772c5eef4b
Jonas Spinner <jonasspinner@gmx.de>
2025-04-12 23:07:06 +0200

    Add ParT block equivariance test

--- tests/nn/test_particletransformer.py
+++ tests/nn/test_particletransformer.py
@@ -4,19 +4,98 @@ from tests.constants import (
     TOLERANCES,
     MILD_TOLERANCES,
     LOGM2_MEAN_STD,
+    REPS,
+    LFRAMES_PREDICTOR,
 )
 from tests.helpers import sample_particle, equivectors_builder
 
-from tensorframes.nn.particletransformer import ParticleTransformer
+from tensorframes.nn.particletransformer import ParticleTransformer, Block
 from tensorframes.reps.tensorreps import TensorReps
 from tensorframes.reps.tensorreps_transform import TensorRepsTransform
 from tensorframes.utils.transforms import rand_lorentz
+from tensorframes.lframes.lframes import InverseLFrames
 from tensorframes.lframes.equi_lframes import (
     LearnedOrthogonalLFrames,
     LearnedPolarDecompositionLFrames,
 )
 
 from experiments.tagging.embedding import get_tagging_features
+
+
+@pytest.mark.parametrize("LFramesPredictor", LFRAMES_PREDICTOR)
+@pytest.mark.parametrize("batch_dims", [[10]])
+@pytest.mark.parametrize("num_heads", [8])
+@pytest.mark.parametrize("attn_reps", REPS)
+@pytest.mark.parametrize("logm2_mean,logm2_std", LOGM2_MEAN_STD)
+def test_block_invariance_equivariance(
+    LFramesPredictor,
+    batch_dims,
+    logm2_std,
+    logm2_mean,
+    attn_reps,
+    num_heads,
+):
+    dtype = torch.float64
+
+    assert len(batch_dims) == 1
+    equivectors = equivectors_builder()
+    predictor = LFramesPredictor(equivectors=equivectors).to(dtype=dtype)
+    call_predictor = lambda fm: predictor(fm)
+
+    # define edgeconv
+    in_reps = TensorReps("1x1n")
+    attn_reps = TensorReps(attn_reps)
+    trafo = TensorRepsTransform(TensorReps(in_reps))
+    linear_in = torch.nn.Linear(in_reps.dim, attn_reps.dim * num_heads).to(dtype=dtype)
+    linear_out = torch.nn.Linear(attn_reps.dim * num_heads, in_reps.dim).to(dtype=dtype)
+    ParT_block = Block(attn_reps=attn_reps, embed_dim=attn_reps.dim * num_heads).to(
+        dtype
+    )
+    ParT_block.eval()  # turn off dropout
+
+    def block_wrapper(x, lframes):
+        x = x.unsqueeze(0)
+        mask = torch.ones_like(x[..., 0])
+        lframes = lframes.reshape(1, *lframes.shape)
+        x = ParT_block(x=x, lframes=lframes, padding_mask=mask)
+        x = x.squeeze(0)
+        return x
+
+    # get global transformation
+    random = rand_lorentz([1], dtype=dtype)
+    random = random.repeat(*batch_dims, 1, 1)
+
+    # sample Lorentz vectors
+    fm = sample_particle(batch_dims, logm2_std, logm2_mean, dtype=dtype)
+    lframes = call_predictor(fm)
+    fm_local = trafo(fm, lframes)
+
+    # edgeconv - global
+    x_local = linear_in(fm_local)
+    x_prime_local = block_wrapper(x_local, lframes)
+    fm_prime_local = linear_out(x_prime_local)
+    # back to global
+    fm_prime_global = trafo(fm_prime_local, InverseLFrames(lframes))
+    fm_prime_tr_global = torch.einsum("...ij,...j->...i", random, fm_prime_global)
+
+    # global - edgeconv
+    fm_transformed = torch.einsum("...ij,...j->...i", random, fm)
+    lframes_transformed = call_predictor(fm_transformed)
+    fm_tr_local = trafo(fm_transformed, lframes_transformed)
+    x_tr_local = linear_in(fm_tr_local)
+    x_tr_prime_local = block_wrapper(x_tr_local, lframes_transformed)
+    fm_tr_prime_local = linear_out(x_tr_prime_local)
+    # back to global frame
+    fm_tr_prime_global = trafo(fm_tr_prime_local, InverseLFrames(lframes_transformed))
+
+    # test feature invariance before the operation
+    torch.testing.assert_close(x_local, x_tr_local, **TOLERANCES)
+
+    # test feature invariance after the operation
+    torch.testing.assert_close(x_tr_prime_local, x_prime_local, **TOLERANCES)
+
+    # test equivariance of outputs
+    torch.testing.assert_close(fm_tr_prime_global, fm_prime_tr_global, **TOLERANCES)
 
 
 @pytest.mark.parametrize(
