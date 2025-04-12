8b0cc6925b3765779d610d4ae49dc124145c5e64
Jonas Spinner <jonasspinner@gmx.de>
2025-04-12 12:16:44 +0200

    Add flop-counting tests

---
+++ tests_exp/test_amplitudes_flops.py
@@ -0,0 +1,62 @@
+import pytest
+import hydra
+from torch.utils.flop_counter import FlopCounterMode
+
+import experiments.logger
+from experiments.amplitudes.experiment import AmplitudeExperiment
+
+
+@pytest.mark.parametrize(
+    "lframesnet",
+    [
+        "identity",
+        "randomrotation",
+        "randomlorentz",
+        "orthogonal",
+        "polardec",
+    ],
+)
+@pytest.mark.parametrize(
+    "model_list",
+    [
+        ["model=amp_mlp"],
+        ["model=amp_transformer"],
+        ["model=amp_graphnet"],
+        ["model=amp_graphnet", "model.include_edges=false"],
+        ["model=amp_graphnet", "model.include_nodes=false"],
+    ],
+)
+@pytest.mark.parametrize("iterations", [1])
+def test_amplitudes(lframesnet, model_list, iterations):
+    experiments.logger.LOGGER.disabled = True  # turn off logging
+
+    # create experiment environment
+    with hydra.initialize(config_path="../config", version_base=None):
+        overrides = [
+            *model_list,
+            f"model/lframesnet={lframesnet}",
+            "save=false",
+            "training.batchsize=1",
+        ]
+        cfg = hydra.compose(config_name="amplitudes", overrides=overrides)
+        exp = AmplitudeExperiment(cfg)
+    exp._init()
+    exp.init_physics()
+    exp.init_model()
+    exp.init_data()
+    exp._init_dataloader()
+    exp._init_loss()
+
+    for i, data in enumerate(exp.train_loader):
+        mom = data[1].to(device=exp.device)
+        if i == iterations:
+            break
+        with FlopCounterMode(display=False) as flop_counter:
+            exp.model(mom)
+            flops = flop_counter.get_total_flops()
+
+    print(
+        f"flops (batchsize=1): {flops:.2e}",
+        model_list,
+        lframesnet,
+    )
---
+++ tests_exp/test_tagging_flops.py
@@ -0,0 +1,62 @@
+import pytest
+import hydra
+from torch.utils.flop_counter import FlopCounterMode
+
+import experiments.logger
+from experiments.tagging.experiment import TopTaggingExperiment
+
+
+@pytest.mark.parametrize(
+    "lframesnet",
+    [
+        "identity",
+        "randomrotation",
+        "randomlorentz",
+        "orthogonal",
+        "polardec",
+    ],
+)
+@pytest.mark.parametrize(
+    "model_list",
+    [
+        ["model=tag_particlenet-lite"],
+        ["model=tag_transformer"],
+        ["model=tag_graphnet"],
+        ["model=tag_graphnet", "model.include_edges=false"],
+    ],
+)
+@pytest.mark.parametrize("iterations", [1])
+def test_tagging(lframesnet, model_list, iterations):
+    experiments.logger.LOGGER.disabled = True  # turn off logging
+
+    # create experiment environment
+    # note: use the full models
+    with hydra.initialize(config_path="../config", version_base=None):
+        overrides = [
+            *model_list,
+            f"model/lframesnet={lframesnet}",
+            "save=false",
+            "training.batchsize=1",
+        ]
+        cfg = hydra.compose(config_name="toptagging", overrides=overrides)
+        exp = TopTaggingExperiment(cfg)
+    exp._init()
+    exp.init_physics()
+    exp.init_model()
+    exp.init_data()
+    exp._init_dataloader()
+    exp._init_loss()
+
+    for i, data in enumerate(exp.train_loader):
+        if i == iterations:
+            break
+
+        with FlopCounterMode(display=False) as flop_counter:
+            exp._get_ypred_and_label(data)
+            flops = flop_counter.get_total_flops()
+
+    print(
+        f"flops (batchsize=1): {flops:.2e}",
+        model_list,
+        lframesnet,
+    )
