bc4f5df6aeed79dabb2c2bf70380010f278f8c2c
Jonas Spinner <jonasspinner@gmx.de>
2025-04-27 09:09:11 +0200

    Modify sweep

--- sweep/sweep-eg1.py
+++ sweep/sweep-eg1.py
@@ -21,11 +21,6 @@ def run_trial(trial: Trial, seed, exp_name, model, cfg_overrides):
         "beam_reference", ["lightlike", "timelike"]
     )
     two_beams = trial.suggest_categorical("two_beams", ["true", "false"])
-    # lframesnet
-    eps_norm = 10 ** trial.suggest_int("log10_eps_norm", -15, -4)
-    eps_reg_coplanar = 10 ** trial.suggest_int("log10_eps_reg_coplanar", -15, -4)
-    eps_reg_lightlike = 10 ** trial.suggest_int("log10_eps_reg_lightlike", -15, -4)
-    eps_reg = 10 ** trial.suggest_int("log10_eps_reg", -15, -4)
     # equigraph
     hidden_channels = 2 ** trial.suggest_int("log2_hidden_channels", 4, 7)
     hidden_layers_mlp = trial.suggest_int("hidden_layers_mlp", 1, 4)
@@ -60,23 +55,12 @@ def run_trial(trial: Trial, seed, exp_name, model, cfg_overrides):
             f"model/lframesnet={lframesnet}",
             f"data.spurions.beam_reference={beam_reference}",
             f"data.spurions.two_beams={two_beams}",
-            f"model.lframesnet.ortho_kwargs.eps_norm={eps_norm}",
             f"model.lframesnet.equivectors.hidden_channels={hidden_channels}",
             f"model.lframesnet.equivectors.num_layers_mlp={hidden_layers_mlp}",
             f"model.lframesnet.equivectors.nonlinearity={nonlinearity}",
-            f"model.lframesnet.ortho_kwargs.eps_norm={eps_norm}",
             f"training.lr_factor_lframesnet={lr_factor_lframesnet}",
             f"training.clip_grad_norm={clip_grad_norm}",
         ]
-        if lframesnet == "orthogonal":
-            overrides += [
-                f"model.lframesnet.ortho_kwargs.eps_reg_coplanar={eps_reg_coplanar}",
-                f"model.lframesnet.ortho_kwargs.eps_reg_lightlike={eps_reg_lightlike}",
-            ]
-        elif lframesnet == "polardec":
-            overrides += [
-                f"model.lframesnet.ortho_kwargs.eps_reg={eps_reg}",
-            ]
 
         cfg = compose(config_name="ttbar", overrides=overrides)
         try:
