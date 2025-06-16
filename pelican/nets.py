d9f494557d5163838c9b7d23a7838f6bf0e263f4
Jonas Spinner <jonasspinner@gmx.de>
2025-06-16 12:17:29 +0200

    Redesign input combination

--- pelican/nets.py
+++ pelican/nets.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 from torch.utils.checkpoint import checkpoint
 
@@ -30,31 +31,35 @@ class PELICAN(nn.Module):
         super().__init__()
 
         # embed inputs into edge features
-        assert in_rank2 > 0
-        self.in_aggregator2 = Aggregator2to2(
-            in_channels=in_rank2,
+        self.in_aggregator_rank1 = (
+            Aggregator1to2(
+                in_channels=in_rank1,
+                out_channels=in_rank1,
+                factorize=factorize,
+                aggr=aggr,
+            )
+            if in_rank1 > 0
+            else None
+        )
+        self.in_aggregator_rank0 = (
+            Aggregator0to2(
+                in_channels=in_rank0,
+                out_channels=in_rank0,
+                factorize=factorize,
+                aggr=aggr,
+            )
+            if in_rank0 > 0
+            else None
+        )
+
+        in_rank2_effective = in_rank2 + in_rank1 + in_rank0
+        assert in_rank2_effective > 0
+        self.in_aggregator_rank2 = Aggregator2to2(
+            in_channels=in_rank2_effective,
             out_channels=hidden_channels,
             factorize=factorize,
             aggr=aggr,
         )
-        if in_rank1 > 0:
-            self.in_aggregator1 = Aggregator1to2(
-                in_channels=in_rank1,
-                out_channels=hidden_channels,
-                factorize=factorize,
-                aggr=aggr,
-            )
-        else:
-            self.input_aggregator1 = None
-        if in_rank0 > 0:
-            self.in_aggregator0 = Aggregator0to2(
-                in_channels=in_rank0,
-                out_channels=hidden_channels,
-                factorize=factorize,
-                aggr=aggr,
-            )
-        else:
-            self.input_aggregator0 = None
 
         # process edge features
         self._checkpoint_blocks = checkpoint_blocks
@@ -98,13 +103,15 @@ class PELICAN(nn.Module):
 
     def forward(self, in_rank2, edge_index, batch, in_rank1=None, in_rank0=None):
         # embed inputs into edge features
-        x = self.in_aggregator2(in_rank2, edge_index, batch)
-        if in_rank1 is not None:
-            edges_fromrank1 = self.in_aggregator1(in_rank1, edge_index, batch)
-            x += edges_fromrank1
-        if in_rank0 is not None:
-            edges_fromrank0 = self.in_aggregator0(in_rank0, edge_index, batch)
-            x += edges_fromrank0
+        edges = [in_rank2]
+        if in_rank1 is not None and self.in_aggregator_rank1 is not None:
+            edges_fromrank1 = self.in_aggregator_rank1(in_rank1, edge_index, batch)
+            edges.append(edges_fromrank1)
+        if in_rank0 is not None and self.in_aggregator_rank0 is not None:
+            edges_fromrank0 = self.in_aggregator_rank0(in_rank0, edge_index, batch)
+            edges.append(edges_fromrank0)
+        edges = torch.cat(edges, dim=-1)
+        x = self.in_aggregator_rank2(edges, edge_index, batch)
 
         # process edge features
         for block in self.blocks:
