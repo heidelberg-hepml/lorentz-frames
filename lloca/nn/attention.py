f313d90c4d0a9b9aba071765d1710fafff1cf9b1
Jonas Spinner <jonasspinner@gmx.de>
2025-07-18 14:13:21 +0200

    Add LayerNorm to LLoCaAttention

--- lloca/nn/attention.py
+++ lloca/nn/attention.py
@@ -122,6 +122,9 @@ class LLoCaAttention(torch.nn.Module):
 
         qkv_local = torch.stack([q_local, k_local, v_local], dim=0)
         qkv_global = self.transform(qkv_local, self.lframes_qkv)
+        qkv_global = torch.nn.functional.layer_norm(
+            qkv_global, normalized_shape=qkv_global.shape[-1:]
+        )
         q_global, k_global, v_global = torch.unbind(qkv_global, dim=0)
 
         # (B, H, N, C) format required for scaled_dot_product_attention
@@ -142,6 +145,9 @@ class LLoCaAttention(torch.nn.Module):
 
         # transform out back into local frame
         out_local = self.transform(out_global, self.lframes)
+        out_local = torch.nn.functional.layer_norm(
+            out_local, normalized_shape=out_local.shape[-1:]
+        )
         return out_local
 
 
