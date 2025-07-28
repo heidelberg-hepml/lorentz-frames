8bdde5daaf841d2604281a9ec8edc8a8419367c0
Jonas Spinner <jonasspinner@gmx.de>
2025-07-28 12:19:24 +0200

    Add MIParT

--- experiments/tagging/experiment.py
+++ experiments/tagging/experiment.py
@@ -101,7 +101,10 @@ class TaggingExperiment(BaseExperiment):
         )
 
     def _init_optimizer(self, param_groups=None):
-        if self.cfg.model.net._target_.rsplit(".", 1)[-1] == "ParticleTransformer":
+        if self.cfg.model.net._target_.rsplit(".", 1)[-1] in [
+            "ParticleTransformer",
+            "MIParticleTransformer",
+        ]:
             # special treatment for ParT, see
             # https://github.com/hqucms/weaver-core/blob/dev/custom_train_eval/weaver/train.py#L464
             # have to adapt this for finetuning!!!
